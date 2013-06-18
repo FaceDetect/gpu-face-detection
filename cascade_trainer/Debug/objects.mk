6f0b8a16fee139036513fe72b3b9985bd3950d31
farenheit466@gmail.com <farenheit466@gmail.com@304495fc-d9ce-8193-7d39-268600517cdf>
2013-06-18 20:27:54 +0000

    makefiles added

---
+++ cascade_trainer/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_core -lopencv_contrib -lgpu_obj_detection -lopencv_imgproc -lopencv_highgui
+
